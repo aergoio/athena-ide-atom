7ad43684428ef1387ed4701fb785c174dca81c11
Taeik Lim <sibera21@gmail.com>
2019-01-10 17:12:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# athena-ide package
+
+A package for writing smart contract in aergo using atom ide
---
+++ spec/athena-ide-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AthenaIdeView from '../lib/athena-ide-view';
+
+describe('AthenaIdeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
