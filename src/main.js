45d98fd57f7fe10783e135e2c61a3700d827f031
Taeik Lim <sibera21@gmail.com>
2019-02-14 17:46:54 +0900

    Apply console log ui

--- src/main.js
+++ src/main.js
@@ -4,45 +4,42 @@
 
 import { CompositeDisposable } from 'atom';
 
-import CompileView from './view/compile-view';
+import ConsoleView from './view/console-view';
 import LuaAutocompleteProvider from './lua-autocomplete-provider';
 import LuaLintProvider from './lua-lint-provider';
 import LuaCompiler from './lua-compiler';
 
 export default {
 
-  compileView: null,
-  subscriptions: null,
-
   activate(state) {
-    // compile view
-    this.compileView = new CompileView();
-
-    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.consoleView = new ConsoleView();
     this.subscriptions = new CompositeDisposable();
-    
-    // Register command that toggles this view
-    this.subscriptions.add(atom.commands.add('atom-text-editor', {
-      'athena-ide:compile': () => {
-        const filePath = atom.workspace.getActiveTextEditor().getBuffer().getPath();
-        const source = atom.workspace.getActiveTextEditor().getBuffer().getText();
-        const compileResult = new LuaCompiler().compile(source, filePath);
-        this.compileView.attach();
-        if (compileResult.result === "success") {
-          this.compileView.setSuccess(compileResult.message);
-        } else {
-          this.compileView.setFail(compileResult.message);
-        }
-      }
-    }));
-    this.subscriptions.add(atom.workspace.observeTextEditors(editor => {
-      editor.onDidSave(path => console.log(path));
-    }));
+    this._addSubscriptions(this.subscriptions);
   },
 
   deactivate() {
     this.subscriptions.dispose();
-    this.compileView.destroy();
+    this.consoleView.distroy();
+  },
+
+  _addSubscriptions(subscriptions) {
+    subscriptions.add(atom.commands.add('atom-text-editor', {
+      'athena-ide:compile': () => {
+        const filePath = atom.workspace.getActiveTextEditor().getBuffer().getPath();
+        const source = atom.workspace.getActiveTextEditor().getBuffer().getText();
+        const compileResult = new LuaCompiler().compile(source, filePath);
+        this.consoleView.show().then((view) => {
+          if (compileResult.result === "success") {
+            view.log({data: compileResult.message, level: "info"});
+          } else {
+            view.log({data: compileResult.message, level: "error"});
+          }
+        });
+      }
+    }));
+    subscriptions.add(atom.workspace.observeTextEditors(editor => {
+      editor.onDidSave(path => console.log(path));
+    }));
   },
 
   getProvider () {
---
+++ src/view/console-view.js
@@ -0,0 +1,66 @@
+'use babel';
+
+/* eslint-disable */
+
+import {$$, View} from 'atom-space-pen-views';
+
+export default class ConsoleView extends View {
+
+  static content() {
+    this.div({id: 'athena-ide-console'}, () => {
+      this.div({class: 'panel-body view-scroller', outlet: 'body'}, () => {
+        this.pre({class: 'native-key-bindings', outlet: 'output', tabindex: -1});
+      });
+    });
+  }
+
+  show() {
+    atom.workspace.getBottomDock().show();
+    return atom.workspace.open(this, {activatePane: false});
+  }
+
+  distroy() {
+    // TODO : remove view from the bottom dock
+    this.clear();
+  }
+
+  getTitle() {
+    return 'Athena Ide Console';
+  }
+
+  getPath() {
+    return 'console-view';
+  }
+
+  getUri() {
+    return `particle-dev://editor/${this.getPath()}`;
+  }
+
+  getDefaultLocation() {
+    return 'bottom';
+  }
+
+  log(message) {
+    const data = message.data.toString();
+    const level = message.level;
+    const dataWithTime = this._wrapTime(data);
+    this.output.append($$(function() {
+      this.div({class: `level-${level}`}, dataWithTime);
+    }));
+    this.body.scrollToBottom();
+  }
+
+  _wrapTime(message) {
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    const timeInfo = [ hour, minute, second ].map((m) => m < 10 ? "0" + m : m).join(":");
+    return timeInfo + " " + message.toString();
+  }
+
+  clear() {
+    this.output.empty();
+  }
+
+}
