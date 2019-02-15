1f88fccd1820028df1a7039a1b2df1131bed11ac
Taeik Lim <sibera21@gmail.com>
2019-02-15 20:37:49 +0900

    Fix bug unsuggesting just after the (, ), {, }

--- src/view/autocomplete-provider.js
+++ src/view/autocomplete-provider.js
@@ -3,6 +3,8 @@
 import logger from '../logger';
 import * as adaptor from './type-adaptor';
 
+const prefixParsingEnd = [";", "(", ")", "{", "}"];
+
 export default class AutoCompleteProvider {
 
   constructor() {
@@ -20,53 +22,39 @@ export default class AutoCompleteProvider {
     logger.debug("Resolve suggestions with");
     logger.debug(request);
 
+    // resolve origin prefix info in editor
     const textBuffer = request.editor.getBuffer();
     const cursorPosition = request.bufferPosition;
     const originPrefix = request.prefix;
     const originPrefixEndIndex = textBuffer.characterIndexForPosition(request.bufferPosition);
     const originPrefixStartIndex = originPrefixEndIndex - originPrefix.length;
-    logger.debug("origin prefix: " + originPrefix +
-          " (start: " + originPrefixStartIndex + ", end: " + originPrefixEndIndex + ")");
+    logger.debug("origin prefix")
+    logger.debug(originPrefix);
+    logger.debug("start")
+    logger.debug(originPrefixStartIndex);
+    logger.debug("end")
+    logger.debug(originPrefixEndIndex );
 
-    const currentLine = textBuffer.lineForRow(cursorPosition.row);
+    // parse prefix info in line
     const currentLineStartIndex = textBuffer.characterIndexForPosition({row: cursorPosition.row, column: 0});
-    logger.debug("current line start index: " + currentLineStartIndex);
+    const originPrefixStart = originPrefixStartIndex - currentLineStartIndex;
+    const currentLine = textBuffer.lineForRow(cursorPosition.row);
+    const prefixInfo = this._resolvePrefix(originPrefix, originPrefixStart, currentLine);
+    logger.debug("parsed prefix")
+    logger.info(prefixInfo)
 
-    let buffer = originPrefix;
-    let cannotFillBuffer = false;
-    let index = originPrefixStartIndex - currentLineStartIndex - 1;
-    let prefix = "";
-    logger.debug("member tracking from: " + index);
-    while (index >= 0 && currentLine[index] !== ";") {
-      if (currentLine[index] === " ") {
-        if (buffer.length > 0) {
-          cannotFillBuffer = true;
-        }
-      } else if (currentLine[index] === ".") {
-        if (0 === buffer.length) break;
-        prefix = "." + buffer + prefix;
-        buffer = "";
-        cannotFillBuffer = false;
-      } else {
-        // meet character after whitespace when buffer is filled
-        if (cannotFillBuffer) break;
-        buffer = currentLine[index] + buffer;
-      }
-      --index;
-    }
-    prefix = buffer + prefix;
-
+    // resolve parsed prefix info in editor
     const textInIndex = (startIndex, endIndex) => {
       const range = [textBuffer.positionForCharacterIndex(startIndex), textBuffer.positionForCharacterIndex(endIndex)];
       return textBuffer.getTextInRange(range);
     }
-    const prefixStartIndex = currentLineStartIndex + index + 1;
+    const prefixStartIndex = currentLineStartIndex + prefixInfo.prefixStart;
     const prefixEndIndex = originPrefixEndIndex;
     const lastSourceIndex = textBuffer.getMaxCharacterIndex();
     const source = textInIndex(0, prefixStartIndex) + textInIndex(prefixEndIndex, lastSourceIndex);
     const filePath = textBuffer.getPath();
 
-    return this.services.autoCompleteService.suggest(source, filePath, prefix, prefixStartIndex).then(rawSuggestions => {
+    return this.services.autoCompleteService.suggest(source, filePath, prefixInfo.prefix, prefixStartIndex).then(rawSuggestions => {
       logger.debug("Raw suggestions");
       logger.debug(rawSuggestions);
       const atomSuggestions = rawSuggestions.map(suggestion => adaptor.adaptSuggestionToAtom(suggestion));
@@ -74,6 +62,37 @@ export default class AutoCompleteProvider {
       logger.info(atomSuggestions);
       return atomSuggestions;
     });
+  }
+
+  _resolvePrefix(originPrefix, originPrefixStart, line) {
+    let prefix = "";
+    let buffer = originPrefix;
+    let cannotFillBuffer = false;
+    let index = originPrefixStart - 1;
+    logger.debug("member tracking from: " + index);
+    while (index >= 0 && prefixParsingEnd.indexOf(line[index]) === -1) {
+      if (line[index] === " ") {
+        if (buffer.length > 0) {
+          cannotFillBuffer = true;
+        }
+      } else if (line[index] === ".") {
+        if (0 === buffer.length) break;
+        prefix = "." + buffer + prefix;
+        buffer = "";
+        cannotFillBuffer = false;
+      } else {
+        // meet character after whitespace when buffer is filled
+        if (cannotFillBuffer) break;
+        buffer = line[index] + buffer;
+      }
+      --index;
+    }
+    prefix = buffer + prefix;
+
+    return {
+      prefix: prefix,
+      prefixStart: index + 1
+    };
   }
 
 }
