c43046a637cc6eb218972e8f3004d272183177c9
Taeik Lim <sibera21@gmail.com>
2019-10-17 17:52:26 +0900

    Set rebuild target as grpc

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,8 +2,8 @@
 ## check release notes of atom : https://github.com/atom/atom/releases
 
 # up to 1.28.0
-npm rebuild --runtime=electron --target=2.0.0
+npm rebuild --runtime=electron --target=2.0.0 grpc
 # up to 1.39.0
-npm rebuild --runtime=electron --target=3.1.1
+npm rebuild --runtime=electron --target=3.1.1 grpc
 # up to 1.41.0
-npm rebuild --runtime=electron --target=4.2.0
+npm rebuild --runtime=electron --target=4.2.0 grpc
